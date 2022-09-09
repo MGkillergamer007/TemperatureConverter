408cb0fbe88c2f142819ca752d1a9e62b79746f6
MGkillergamer007 <81156826+MGkillergamer007@users.noreply.github.com>
2022-09-09 22:45:58 +0300

    fixed header label

--- gui/main_gui.py
+++ gui/main_gui.py
@@ -2,7 +2,7 @@ from tkinter import *
 
 root = Tk()
 
-label = Label(root, text="Temperature Converter!").grid(row=0, column=0)
+label = Label(root, text="Temperature Converter").grid(row=0, column=0)
 label2 = Label(root, text="by MGkillergamer, TheDʌRkW0lF").grid(row=1, column=0)
 
 root.mainloop()
